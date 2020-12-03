368d6a4a281d97d268dc61d9c802789f19716590
HERNANDEZ, MAURO GABRIEL <mauro.hernandez@despegar.com>
2020-12-03 09:19:29 -0300

    Agrego el id a los claims

--- src/main/java/com/grupo6/dssd/controller/remote/UserController.java
+++ src/main/java/com/grupo6/dssd/controller/remote/UserController.java
@@ -57,6 +57,7 @@ public class UserController {
 
 		String token = Jwts.builder().setId(Constant.SECRET_KEY).setSubject(user.getName())
 		        .claim("role", user.getRole().getName())
+		        .claim("id", user.getId())
 //		                grantedAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
 		        .setIssuedAt(new Date(System.currentTimeMillis()))
 		        .setExpiration(new Date(System.currentTimeMillis() + 1200000))
